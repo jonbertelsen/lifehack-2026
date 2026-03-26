1507477b5528e8b6c12ed0516c5ddde859932e68
ErosiaBjerrum <anders.bjerrum86@gmail.com>
2026-03-26 12:41:49 +0100

    All options added

--- src/main/java/app/controllers/SpinTheWheelController.java
+++ src/main/java/app/controllers/SpinTheWheelController.java
@@ -22,6 +22,9 @@ public class SpinTheWheelController {
         // TODO: get options
         String option1 = ctx.formParam("option1");
         String option2 = ctx.formParam("option2");
+        String option3 = ctx.formParam("option3");
+        String option4 = ctx.formParam("option4");
+        String option5 = ctx.formParam("option5");
 
         List<String> options = new ArrayList<>();
 
@@ -32,6 +35,23 @@ public class SpinTheWheelController {
         if (!option2.isEmpty()){
             options.add(option2);
         }
+
+        if (!option3.isEmpty()){
+            options.add(option1);
+        }
+
+        if (!option4.isEmpty()){
+            options.add(option2);
+        }
+
+        if (!option5.isEmpty()){
+            options.add(option1);
+        }
+
+
+
+
+
         // TODO: pick a random option
         Random random = new Random();
         int choice = random.nextInt(0, options.size());
