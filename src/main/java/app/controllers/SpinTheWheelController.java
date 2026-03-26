e19ff63fdb58d699f10a9b9da3e65ffb2896cbca
Jon Bertelsen <jobe@cphbusiness.dk>
2026-03-26 12:36:14 +0100

    add getResult in controller

--- src/main/java/app/controllers/SpinTheWheelController.java
+++ src/main/java/app/controllers/SpinTheWheelController.java
@@ -3,6 +3,10 @@ package app.controllers;
 import app.persistence.SpinTheWheelMapper;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class SpinTheWheelController {
     SpinTheWheelMapper spinTheWheelMapper;
 
@@ -16,8 +20,23 @@ public class SpinTheWheelController {
 
     public void getResult(Context ctx){
         // TODO: get options
+        String option1 = ctx.formParam("option1");
+        String option2 = ctx.formParam("option2");
+
+        List<String> options = new ArrayList<>();
+
+        if (!option1.isEmpty()){
+            options.add(option1);
+        }
+
+        if (!option2.isEmpty()){
+            options.add(option2);
+        }
         // TODO: pick a random option
-        ctx.attribute("result", "Denne option er valg: option 1");
+        Random random = new Random();
+        int choice = random.nextInt(0, options.size());
+
+        ctx.attribute("result", options.get(choice));
         ctx.render("stw/result.html");
     }
 
